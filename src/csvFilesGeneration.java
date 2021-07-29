628b5a51eaae6a503f53a5533b6eef014b4e9a75
POHNAH <wolf.programmer@gmail.com>
2021-07-29 10:03:30 +0300

    Примитив

---
+++ src/csvFilesGeneration.java
@@ -0,0 +1,23 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class csvFilesGeneration {
+    public static void main(String[] args) {
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter("/home/avolkov/downloads/skuId.csv")))
+        {
+
+            for (int i = 0; i < 5000; i++) {
+                bw.write(String.format("%6d",i).replaceAll(" ","0") + ";\n");
+            }
+//            String text = "Hello  World!\nHey! Teachers! Leave them kids alone.";
+
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+
+    }
+}
